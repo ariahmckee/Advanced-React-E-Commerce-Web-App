7119c6081f4e0ffd0768c3165b570652538f55e4
Ariah <thehittite@gmail.com>
2026-06-18 10:12:09 -0700

    Wired Redux and React Query providers to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import "./styles.css";
 import { CartProvider } from "./context/CartContext.jsx";
 import { AuthProvider } from "./context/AuthContext";
+import { store } from "./store/store";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </AuthProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </AuthProvider>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>,
 );
